b9ebe87ad9565007721ae20dabe0120220bdfdc3
Sowparna Shome <soupornoshome@gmail.com>
2025-01-09 23:29:42 +0530

    wrapper add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Wrapper from "./components/Wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,13 +22,16 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>)
+{
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Wrapper>
+          {children}
+          </Wrapper>
       </body>
     </html>
   );
